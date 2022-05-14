73fb8965bfe63bd4202bcd5d5d0281cbf27ee832
rsookram <rashadsookram@gmail.com>
2022-05-13 21:14:28 -0400

    Fill in help output

--- src/opt.rs
+++ src/opt.rs
@@ -1,3 +1,5 @@
+//! Handling of command line arguments.
+
 use anyhow::anyhow;
 use anyhow::bail;
 use anyhow::Result;
@@ -6,35 +8,48 @@ use std::convert::Infallible;
 use std::path::PathBuf;
 use std::process;
 
-/// Contains parsed command line arguments.
+/// A parsed representation of command line arguments.
 #[derive(Debug)]
 pub struct Opt {
-    /// The subcommand to run.
+    /// The [Subcommand] to run.
     pub subcommand: Subcommand,
 
-    /// The path of the database file. Defaults to srs.db.
+    /// The path of the database file. Defaults to ./srs.db.
     pub path: PathBuf,
 }
 
+/// The subcommand to run.
 #[derive(Debug)]
 pub enum Subcommand {
+    /// Create a new card, and add it to the deck with the given ID.
     Add { deck_id: u64 },
+    /// List all cards in all decks.
     Cards,
+    /// Create a new deck with the given name.
     CreateDeck { name: String },
+    /// List all decks.
     Decks,
+    /// Delete the card with the given ID.
     Delete { card_id: u64 },
+    /// Delete the deck with the given ID, including all of its cards.
     DeleteDeck { deck_id: u64 },
+    /// Edit the contents of the card with the given ID.
     Edit { card_id: u64 },
+    /// Create an empty srs-cli database.
     Init,
+    /// Set the interval modifier of the deck with the given ID.
     IntMod { deck_id: u64, modifier: u16 },
+    /// Review cards that are scheduled for review.
     Review,
+    /// Output statistics of decks and reviews.
     Stats,
+    /// Move a card to the deck with the given ID.
     Switch { card_id: u64, deck_id: u64 },
 }
 
 impl Opt {
-    /// Gets [Opt] from the command line arguments. Prints the error message
-    /// and quits the program in case of failure.
+    /// Gets [Opt] from the command line arguments. Prints the error message and quits the program
+    /// in case of failure.
     pub fn from_args() -> Self {
         let mut args = Arguments::from_env();
 
@@ -54,6 +69,8 @@ impl Opt {
         })
     }
 
+    /// Parses [Arguments] into [Opt], resulting in an error when unexpected arguments are
+    /// provided, or expected arguments are missing.
     fn parse(mut args: Arguments) -> Result<Self> {
         let path = args
             .opt_value_from_os_str(["-p", "--path"], |p| Ok::<_, Infallible>(PathBuf::from(p)))?
@@ -93,7 +110,7 @@ impl Opt {
                 card_id: args.value_from_str("--card-id")?,
                 deck_id: args.value_from_str("--deck-id")?,
             },
-            _ => bail!("unknown subcommand"),
+            _ => bail!("unknown subcommand `{subcommand}`"),
         };
 
         let remaining = args.finish();
@@ -107,6 +124,7 @@ impl Opt {
     }
 }
 
+/// Print the help output requested by the -h or --help flag.
 fn print_help() {
     println!(
         r#"{name} {version}
@@ -123,18 +141,18 @@ OPTIONS:
     -p, --path <PATH>    The path of the database file [default: srs.db]
 
 SUBCOMMANDS:
-    add
-    cards
-    create-deck
-    decks
-    delete
-    delete-deck
-    edit
-    init
-    int-mod
-    review
-    stats
-    switch"#,
+    add            Create a new card, and add it to a deck
+    cards          List all cards in all decks
+    create-deck    Create a new deck
+    decks          List all decks
+    delete         Delete a card
+    delete-deck    Delete a deck, including all of its cards
+    edit           Edit the contents of a card
+    init           Create an empty {name} database
+    int-mod        Edit a deck's interval modifier
+    review         Review cards that are scheduled for review
+    stats          View statistics of decks and reviews
+    switch         Move a card between decks"#,
         name = env!("CARGO_PKG_NAME"),
         version = env!("CARGO_PKG_VERSION"),
     );
